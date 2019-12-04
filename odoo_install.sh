79eab1b2c204275393ecb1ae44254e6a5c91adf9
MIrkoSerra <54839685+MIrkoSerra@users.noreply.github.com>
2019-12-04 12:41:45 +0100

    Update odoo_install.sh

--- odoo_install.sh
+++ odoo_install.sh
@@ -7,12 +7,6 @@
 # This script will install Odoo on your Ubuntu 16.04 server. It can install multiple Odoo instances
 # in one Ubuntu because of the different xmlrpc_ports
 #-------------------------------------------------------------------------------
-# Make a new file:
-# sudo nano odoo-install.sh
-# Place this content in it and then make the file executable:
-# sudo chmod +x odoo-install.sh
-# Execute the script to install Odoo:
-# ./odoo-install
 ################################################################################
 
 OE_USER="odoo"
